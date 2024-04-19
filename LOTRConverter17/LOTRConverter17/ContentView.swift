28e33c7ff280b35d615c23cc0b2c686a68f24980
Juan Camilo Victoria <144855187+Juanaco123@users.noreply.github.com>
2024-04-19 09:51:25 -0500

    Set up view elements on main view.

--- LOTRConverter17/LOTRConverter17/ContentView.swift
+++ LOTRConverter17/LOTRConverter17/ContentView.swift
@@ -8,15 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+     @State var showExchangeInfo = false
+    
     var body: some View {
         ZStack {
             // Background image
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
             
             VStack {
                 // Prancing pony image
+                Image(.prancingpony)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
                 
                 // Currency exchange text
-                
+                Text("Currency Exchange")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
                 // Currency conversion section
                 HStack {
                     // Left conversion section
@@ -24,29 +35,59 @@ struct ContentView: View {
                         // Currency
                         HStack {
                             // Currency image
-                            
+                            Image(.silverpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
                             // Currency text
+                            Text("Silver Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
                         }
                         
                         // TextField
+                        Text("TextField")
                     }
                     // Equal sign
+                    Image(systemName: "equal")
+                        .font(.largeTitle)
+                        .foregroundStyle(.white)
+                        .symbolEffect(.pulse)
                     
                     // Right conversion section
                     VStack {
                         // Currency
                         HStack {
                             // Currency text
+                            Text("Gold Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
                             
                             // Currency image
+                            Image(.goldpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
                         }
                         
                         // TextField
+                        Text("TextField")
                     }
                 }
                 
+                Spacer()
+                
                 // Info Button
+                Button {
+                    showExchangeInfo = true
+                } label: {
+                    Image(systemName: "info.circle.fill")
+                        .font(.largeTitle)
+                        .foregroundStyle(.white)
+                }
+                
             }
+//            .border(.blue)
         }
     }
 }
