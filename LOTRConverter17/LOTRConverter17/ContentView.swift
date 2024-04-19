86ca1ae518bb2ffa93bedd2f52649b8099f9b26b
Juan Camilo Victoria <144855187+Juanaco123@users.noreply.github.com>
2024-04-19 07:33:08 -0500

    Set up stacks views. Add .gitignore.

---
+++ LOTRConverter17/LOTRConverter17/ContentView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  LOTRConverter17
+//
+//  Created by Juan Camilo Victoria Pacheco on 19/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            // Background image
+            
+            VStack {
+                // Prancing pony image
+                
+                // Currency exchange text
+                
+                // Currency conversion section
+                HStack {
+                    // Left conversion section
+                    VStack {
+                        // Currency
+                        HStack {
+                            // Currency image
+                            
+                            // Currency text
+                        }
+                        
+                        // TextField
+                    }
+                    // Equal sign
+                    
+                    // Right conversion section
+                    VStack {
+                        // Currency
+                        HStack {
+                            // Currency text
+                            
+                            // Currency image
+                        }
+                        
+                        // TextField
+                    }
+                }
+                
+                // Info Button
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LOTRConverter17/LOTRConverter17/LOTRConverter17App.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverter17App.swift
+//  LOTRConverter17
+//
+//  Created by Juan Camilo Victoria Pacheco on 19/04/24.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverter17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
