743395f51738a76c341200507e3a226fee06be8e
Juan Camilo Victoria <144855187+Juanaco123@users.noreply.github.com>
2024-04-24 16:05:06 -0500

    Add Currency tip.

--- LOTRConverter17/LOTRConverter17/ContentView.swift
+++ LOTRConverter17/LOTRConverter17/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import TipKit
 
 struct ContentView: View {
     @State var showExchangeInfo = false
@@ -60,6 +61,7 @@ struct ContentView: View {
                         .onTapGesture {
                             showSelectCurrency.toggle()
                         }
+                        .popoverTip(CurrencyTip(), arrowEdge: .bottom)
                         
                         // TextField
                         TextField("Amount", text: $leftAmount)
@@ -124,6 +126,9 @@ struct ContentView: View {
                 }
             }
             
+        }
+        .task {
+            try? Tips.configure()
         }
         .onChange(of: leftAmount) {
             if leftTyping {
---
+++ LOTRConverter17/LOTRConverter17/CurrencyTip.swift
@@ -0,0 +1,15 @@
+//
+//  CurrencyTip.swift
+//  LOTRConverter17
+//
+//  Created by Juan Camilo Victoria Pacheco on 24/04/24.
+//
+
+import TipKit
+
+struct CurrencyTip: Tip {
+    var title = Text("Change Currency")
+    
+    var message: Text? = Text("You can tap de left or right currency to bring up the Select Currency screen.")
+    
+}
